a5383e823909e04225b3d9bb21ff0c6fa3d42748
Simon Skoczylas <github@giftkugel.de>
2024-05-03 19:55:18 +0200

    Add build script and version definition

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+NAME=stopnik
+VERSION=0.1
+OS_VALUES=(windows darwin linux)
+ARCH_VALUES=(amd64 arm64)
+
+function prepare() {
+  if [[ $(git diff --stat) != '' ]]; then
+    echo "Please commit current changes before build"
+    exit 1
+  else
+    GIT_HASH=$(git rev-parse --short=11 HEAD)
+    echo "$NAME $VERSION - $GIT_HASH"
+    echo "$VERSION" > resources/version
+    echo "$GIT_HASH" >> resources/version
+  fi
+  mkdir -p bin
+}
+
+function clean() {
+  echo "Clean up"
+  rm -rf bin
+}
+
+function build() {
+  GO_OS=$1
+  GO_ARCH=$2
+  COGO_ENABLED=0
+  FILE_EXTENSION=""
+  if [[ "$GO_OS" == "windows" ]]; then
+    FILE_EXTENSION=".exe"
+  fi
+  FILE_NAME="$NAME-$GO_ARCH"
+  DIR="$GO_OS-$GO_ARCH"
+  echo "Build $GO_OS $GO_ARCH"
+  GOOS=$GO_OS GOARCH=$GO_ARCH go build -ldflags="-s -w" -o bin/$DIR/$FILE_NAME$FILE_EXTENSION
+  echo "Create SHA256 Sum $GO_OS $GO_ARCH"
+  sha256sum bin/$DIR/$FILE_NAME$FILE_EXTENSION >> bin/$DIR/sha256sum.txt
+  CURRENT_DIR=$(pwd)
+  echo "Package"
+  cd bin/$DIR
+  zip -q -r ../${NAME}-${GO_OS}-${GO_ARCH}.zip ./
+  cd $CURRENT_DIR
+  echo
+}
+
+function build_all() {
+    for os_value in "${OS_VALUES[@]}"
+    do
+      for arch_value in "${ARCH_VALUES[@]}"
+      do
+    	  build $os_value $arch_value
+      done
+    done
+
+}
+
+clean
+prepare
+build_all
+
+echo "Done!"
--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	_ "embed"
 	"log"
 	"os"
 	"stopnik/internal/config"
 	"stopnik/internal/server"
+	"strings"
 )
+
+//go:embed resources/version
+var version []byte
 
 func main() {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+
+	versionLines := strings.Split(string(version), "\n")
+	if len(versionLines) < 2 {
+		panic("Something went wrong")
+	}
+
+	version := versionLines[0]
+	commitHash := versionLines[1]
+
+	logger.Printf("STOPnik %s - %s", version, commitHash)
 
 	currentConfig := config.LoadConfig("config.yml")
 	logger.Printf("%d", currentConfig.Server.Port)
