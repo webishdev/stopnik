dc0d45b8c2a8ecb37e85fbbf44d47ca6ecb75161
Simon Skoczylas <github@giftkugel.de>
2024-05-03 16:01:37 +0200

    Clean up

--- internal/pkce/pkce.go
+++ internal/pkce/pkce.go
@@ -11,9 +11,9 @@ func ValidatePKCE(method CodeChallengeMethod, value string, verifier string) boo
 }
 
 func calculatePKCE(method CodeChallengeMethod, value string) string {
-	valueHash := sha256.Sum256([]byte(value))
 	switch method {
 	case S256:
+		valueHash := sha256.Sum256([]byte(value))
 		return base64.RawURLEncoding.EncodeToString(valueHash[:])
 	default:
 		return value
