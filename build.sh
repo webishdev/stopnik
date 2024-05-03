86d7acc03595b5265d9d1179b497e3ddd16507c9
Simon Skoczylas <github@giftkugel.de>
2024-05-03 20:22:28 +0200

    Improve build script

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+set -euo pipefail
+
+NICE_NAME=STOPnik
 NAME=stopnik
-VERSION=0.1
+
 OS_VALUES=(windows darwin linux)
 ARCH_VALUES=(amd64 arm64)
 
 function prepare() {
+  if [[ "$#" -ne 1 ]]; then
+    echo "Parameters for prepare are missing"
+    echo
+    exit 1
+  fi
+
   if [[ $(git diff --stat) != '' ]]; then
     echo "Please commit current changes before build"
     echo
     exit 1
   else
+    VERSION=$1
     GIT_HASH=$(git rev-parse --short=11 HEAD)
 
     echo "$VERSION" > resources/version
@@ -23,13 +33,20 @@ function prepare() {
 }
 
 function clean() {
-  echo "Clean up"
   rm -rf bin
+  echo "The bin/ directory was cleaned"
+  echo
 }
 
 function build() {
-  GO_OS=$1
-  GO_ARCH=$2
+  if [[ "$#" -ne 3 ]]; then
+    echo "Parameters for build are missing"
+    echo
+    exit 1
+  fi
+  VERSION=$1
+  GO_OS=$2
+  GO_ARCH=$3
   COGO_ENABLED=0
   FILE_EXTENSION=""
   if [[ "$GO_OS" == "windows" ]]; then
@@ -39,9 +56,9 @@ function build() {
   if [[ "$GO_OS" == "darwin" ]]; then
       OS_NAME="macos"
     fi
-  FILE_NAME="$NAME-$GO_ARCH"
+  FILE_NAME="$NAME-$VERSION-$GO_ARCH"
   DIR="$OS_NAME-$GO_ARCH"
-  echo "Build $GO_OS $GO_ARCH"
+  echo "Build $NICE_NAME version $VERSION for $GO_OS $GO_ARCH"
   GOOS=$GO_OS GOARCH=$GO_ARCH go build -ldflags="-s -w" -o bin/$DIR/$FILE_NAME$FILE_EXTENSION
   echo "Create SHA256 Sum $GO_OS $GO_ARCH"
   sha256sum bin/$DIR/$FILE_NAME$FILE_EXTENSION >> bin/$DIR/sha256sum.txt
@@ -54,18 +71,61 @@ function build() {
 }
 
 function build_all() {
-    for os_value in "${OS_VALUES[@]}"
+  if [[ "$#" -ne 1 ]]; then
+      echo "Parameters for build_all are missing"
+      echo
+      exit 1
+    fi
+  for os_value in "${OS_VALUES[@]}"
+  do
+    for arch_value in "${ARCH_VALUES[@]}"
     do
-      for arch_value in "${ARCH_VALUES[@]}"
-      do
-    	  build $os_value $arch_value
-      done
+      build $1 $os_value $arch_value
     done
-
+  done
+  echo "Done!"
 }
 
-clean
-prepare
-build_all
+function task_clean() {
+    clean
+}
 
-echo "Done!"
+function task_build() {
+  if [[ "$#" -ne 1 ]]; then
+    echo "No version argument supplied"
+    echo
+    exit 1
+  fi
+  if [[ ! "$1" =~ ^[0-9].[0-9]$ ]]; then
+    echo "Wrong version format. Should be x.y"
+    echo
+    exit 1
+  fi
+  echo "Building $NICE_NAME version $1"
+  clean
+  prepare $1
+  build_all $1
+}
+
+function task_usage() {
+  cat <<HEREDOC
+usage: $0 <command>
+Available commands:
+
+    clean
+       Cleans the bin directory
+
+    build version
+        Builds the current version of $NICE_NAME
+
+HEREDOC
+  exit 1
+}
+
+cmd="${1:-}"
+shift || true
+case "$cmd" in
+  clean) task_clean "$@";;
+  build) task_build "$@";;
+  *) task_usage ;;
+esac
