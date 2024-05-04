d429a08f709fee30f09615c6a6e1719d2ef6088b
Simon Skoczylas <github@giftkugel.de>
2024-05-04 13:18:16 +0200

    Improve build

--- build.sh
+++ build.sh
@@ -20,7 +20,6 @@ function prepare() {
     echo
     exit 1
   else
-    VERSION=$1
     GIT_HASH=$(git rev-parse --short=11 HEAD)
 
     echo "$NAME $VERSION - $GIT_HASH"
@@ -36,14 +35,13 @@ function clean() {
 }
 
 function build() {
-  if [[ "$#" -ne 3 ]]; then
+  if [[ "$#" -ne 2 ]]; then
     echo "Parameters for build are missing"
     echo
     exit 1
   fi
-  VERSION=$1
-  GO_OS=$2
-  GO_ARCH=$3
+  GO_OS=$1
+  GO_ARCH=$2
   COGO_ENABLED=0
   FILE_EXTENSION=""
   if [[ "$GO_OS" == "windows" ]]; then
@@ -55,12 +53,12 @@ function build() {
     fi
   FILE_NAME="$NAME$VERSION-$GO_ARCH"
   DIR="$OS_NAME-$GO_ARCH"
-  echo "Build $NICE_NAME version $VERSION for $GO_OS $GO_ARCH"
-  GOOS=$GO_OS GOARCH=$GO_ARCH go build -ldflags="-s -w -X 'main.Version=$VERSION'" -o bin/$DIR/$FILE_NAME$FILE_EXTENSION
-  echo "Create SHA256 Sum $GO_OS $GO_ARCH"
+  echo "Build $NICE_NAME version $VERSION ($GIT_HASH) for $GO_OS $GO_ARCH"
+  GOOS=$GO_OS GOARCH=$GO_ARCH go build -ldflags="-s -w -X 'main.Version=$VERSION' -X 'main.GitHash=$GIT_HASH'" -o bin/$DIR/$FILE_NAME$FILE_EXTENSION
+  echo "Create SHA256 sum for $GO_OS $GO_ARCH"
   sha256sum bin/$DIR/$FILE_NAME$FILE_EXTENSION >> bin/$DIR/sha256sum.txt
   CURRENT_DIR=$(pwd)
-  echo "Package"
+  echo "Package into Zip"
   cd bin/$DIR
   zip -q -r ../${NAME}${VERSION}-${OS_NAME}-${GO_ARCH}.zip ./
   cd $CURRENT_DIR
@@ -68,16 +66,11 @@ function build() {
 }
 
 function build_all() {
-  if [[ "$#" -ne 1 ]]; then
-      echo "Parameters for build_all are missing"
-      echo
-      exit 1
-    fi
   for os_value in "${OS_VALUES[@]}"
   do
     for arch_value in "${ARCH_VALUES[@]}"
     do
-      build $1 $os_value $arch_value
+      build $os_value $arch_value
     done
   done
   echo "Build done!"
@@ -88,20 +81,51 @@ function task_clean() {
 }
 
 function task_build() {
+  if [[ "$#" -ne 3 ]]; then
+    echo "No OS, arch or version argument supplied"
+    echo
+    exit 1
+  fi
+  if [[ ! "$1" =~ ^windows$|^linux$|^darwin$ ]]; then
+    echo "Wrong os format. Should be windows, linux or darwin"
+    echo
+    exit 1
+  fi
+  if [[ ! "$2" =~ ^amd64$|^arm64$ ]]; then
+    echo "Wrong os format. Should be amd64 or arm64"
+    echo
+    exit 1
+  fi
+  if [[ ! "$3" =~ ^[0-9].[0-9]$|^ci$ ]]; then
+    echo "Wrong version format. Should be x.y or ci"
+    echo
+    exit 1
+  fi
+  SELECTED_OS=$1
+  SELECTED_ARCH=$2
+  VERSION=$3
+  echo "Building $NICE_NAME version $VERSION"
+  clean
+  prepare $VERSION
+  build $SELECTED_OS $SELECTED_ARCH
+}
+
+function task_build_all() {
   if [[ "$#" -ne 1 ]]; then
     echo "No version argument supplied"
     echo
     exit 1
   fi
-  if [[ ! "$1" =~ ^[0-9].[0-9]$|ci ]]; then
+  if [[ ! "$1" =~ ^[0-9].[0-9]$|^ci$ ]]; then
     echo "Wrong version format. Should be x.y or ci"
     echo
     exit 1
   fi
-  echo "Building $NICE_NAME version $1"
+  VERSION=$1
+  echo "Building $NICE_NAME version $VERSION"
   clean
-  prepare $1
-  build_all $1
+  prepare $VERSION
+  build_all
 }
 
 function task_usage() {
@@ -112,7 +136,9 @@ Available commands:
     clean
        Cleans the bin directory
 
-    build version
+    build os arch version
+
+    build_all version
         Builds the current version of $NICE_NAME
 
 HEREDOC
@@ -124,5 +150,6 @@ shift || true
 case "$cmd" in
   clean) task_clean "$@";;
   build) task_build "$@";;
+  build_all) task_build_all "$@";;
   *) task_usage ;;
 esac
