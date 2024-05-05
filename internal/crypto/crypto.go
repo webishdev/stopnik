b6747180be6cc91d489c3c7936d1727e008cfcb7
Simon Skoczylas <github@giftkugel.de>
2024-05-05 17:53:22 +0200

    Refactor

---
+++ internal/crypto/crypto.go
@@ -0,0 +1,89 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+// https://codereview.stackexchange.com/questions/125846/encrypting-strings-in-golang
+
+func EncryptString(plainText string, keyString string) (cipherTextString string, err error) {
+
+	key := hashTo32Bytes(keyString)
+	encrypted, err := encryptAES(key, []byte(plainText))
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(encrypted), nil
+}
+
+func DecryptString(cryptoText string, keyString string) (plainTextString string, err error) {
+
+	encrypted, err := base64.URLEncoding.DecodeString(cryptoText)
+	if err != nil {
+		return "", err
+	}
+	if len(encrypted) < aes.BlockSize {
+		return "", fmt.Errorf("cipherText too short. It decodes to %v bytes but the minimum length is 16", len(encrypted))
+	}
+
+	decrypted, err := decryptAES(hashTo32Bytes(keyString), encrypted)
+	if err != nil {
+		return "", err
+	}
+
+	return string(decrypted), nil
+}
+
+func encryptAES(key, data []byte) ([]byte, error) {
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// create two 'windows' in to the output slice.
+	output := make([]byte, aes.BlockSize+len(data))
+	iv := output[:aes.BlockSize]
+	encrypted := output[aes.BlockSize:]
+
+	// populate the IV slice with random data.
+	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+
+	// note that encrypted is still a window in to the output slice
+	stream.XORKeyStream(encrypted, data)
+	return output, nil
+}
+
+func decryptAES(key, data []byte) ([]byte, error) {
+	// split the input up in to the IV seed and then the actual encrypted data.
+	iv := data[:aes.BlockSize]
+	data = data[aes.BlockSize:]
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	stream.XORKeyStream(data, data)
+	return data, nil
+}
+
+// hashTo32Bytes will compute a cryptographically useful hash of the input string.
+func hashTo32Bytes(input string) []byte {
+
+	data := sha256.Sum256([]byte(input))
+	return data[0:]
+
+}
--- internal/server/handler/cookie.go
+++ internal/server/handler/cookie.go
@@ -1,15 +1,9 @@
 package handler
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base64"
-	"fmt"
-	"io"
 	"net/http"
 	"stopnik/internal/config"
+	"stopnik/internal/crypto"
 	"stopnik/log"
 )
 
@@ -28,7 +22,7 @@ func DeleteCookie(config *config.Config) http.Cookie {
 func CreateCookie(config *config.Config, username string) (http.Cookie, error) {
 	authCookieName := config.GetAuthCookieName()
 	log.Debug("Creating %s cookie", authCookieName)
-	value, err := encryptString(username, config.GetServerSecret())
+	value, err := crypto.EncryptString(username, config.GetServerSecret())
 	if err != nil {
 		return http.Cookie{}, err
 	}
@@ -49,89 +43,11 @@ func ValidateCookie(currentConfig *config.Config, r *http.Request) (*config.User
 	if cookieError != nil {
 		return &config.User{}, false
 	} else {
-		username, err := decryptString(cookie.Value, currentConfig.GetServerSecret())
+		username, err := crypto.DecryptString(cookie.Value, currentConfig.GetServerSecret())
 		if err != nil {
 			return &config.User{}, false
 		}
 		user, userExists := currentConfig.GetUser(username)
 		return user, userExists
 	}
-}
-
-// https://codereview.stackexchange.com/questions/125846/encrypting-strings-in-golang
-
-func encryptString(plainText string, keyString string) (cipherTextString string, err error) {
-
-	key := hashTo32Bytes(keyString)
-	encrypted, err := encryptAES(key, []byte(plainText))
-	if err != nil {
-		return "", err
-	}
-
-	return base64.URLEncoding.EncodeToString(encrypted), nil
-}
-
-func encryptAES(key, data []byte) ([]byte, error) {
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	// create two 'windows' in to the output slice.
-	output := make([]byte, aes.BlockSize+len(data))
-	iv := output[:aes.BlockSize]
-	encrypted := output[aes.BlockSize:]
-
-	// populate the IV slice with random data.
-	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-
-	// note that encrypted is still a window in to the output slice
-	stream.XORKeyStream(encrypted, data)
-	return output, nil
-}
-
-func decryptString(cryptoText string, keyString string) (plainTextString string, err error) {
-
-	encrypted, err := base64.URLEncoding.DecodeString(cryptoText)
-	if err != nil {
-		return "", err
-	}
-	if len(encrypted) < aes.BlockSize {
-		return "", fmt.Errorf("cipherText too short. It decodes to %v bytes but the minimum length is 16", len(encrypted))
-	}
-
-	decrypted, err := decryptAES(hashTo32Bytes(keyString), encrypted)
-	if err != nil {
-		return "", err
-	}
-
-	return string(decrypted), nil
-}
-
-func decryptAES(key, data []byte) ([]byte, error) {
-	// split the input up in to the IV seed and then the actual encrypted data.
-	iv := data[:aes.BlockSize]
-	data = data[aes.BlockSize:]
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	stream := cipher.NewCFBDecrypter(block, iv)
-
-	stream.XORKeyStream(data, data)
-	return data, nil
-}
-
-// hashTo32Bytes will compute a cryptographically useful hash of the input string.
-func hashTo32Bytes(input string) []byte {
-
-	data := sha256.Sum256([]byte(input))
-	return data[0:]
-
 }
