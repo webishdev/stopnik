347b3e3ba43c0e81b923cdaf9b63521a922f07be
Simon Skoczylas <github@giftkugel.de>
2024-05-10 21:37:44 +0200

    Improve test script

--- test.sh
+++ test.sh
@@ -3,12 +3,67 @@
 
 COVERAGE_DIR=test_coverage
 
-echo "Clean up"
-rm -rf $COVERAGE_DIR
-mkdir -p $COVERAGE_DIR
+function clean() {
+    echo "Clean up"
+    rm -rf $COVERAGE_DIR
+}
 
-echo "Run tests"
-go test ./... -coverprofile $COVERAGE_DIR/coverage.out -covermode count
+function prepare() {
+  mkdir -p $COVERAGE_DIR
 
-echo "Create Coverage HTML"
-go tool cover -html=$COVERAGE_DIR/coverage.out -o $COVERAGE_DIR/test-coverage.html
+  echo "Run tests"
+  go test ./... -coverprofile $COVERAGE_DIR/coverage.out -covermode count
+}
+
+function html() {
+  echo "Create Coverage HTML"
+  go tool cover -html=$COVERAGE_DIR/coverage.out -o $COVERAGE_DIR/test-coverage.html
+}
+
+function coverage() {
+  echo "Show Coverage"
+  go tool cover -func=$COVERAGE_DIR/coverage.out
+}
+
+function task_clean() {
+  clean
+}
+
+function task_html() {
+  clean
+  prepare
+  html
+}
+
+function task_default() {
+  clean
+  prepare
+  coverage
+}
+
+function task_usage() {
+  cat <<HEREDOC
+usage: $0 <command>
+Available commands:
+
+    clean
+       Cleans the coverage directory
+
+    html
+       Test + HTML Report
+
+    coverage
+        Test + Coverage
+
+HEREDOC
+  exit 1
+}
+
+cmd="${1:-}"
+shift || true
+case "$cmd" in
+  clean) task_clean "$@";;
+  html) task_html "$@";;
+  coverage) task_default ;;
+  *) task_usage ;;
+esac
