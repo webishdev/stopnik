023fe48f51067c9415414cd7fc406138e190f981
Simon Skoczylas <github@giftkugel.de>
2024-05-10 17:09:36 +0200

    Improve test script

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+
+COVERAGE_DIR=test_coverage
+
+echo "Clean up"
+rm -rf $COVERAGE_DIR
+mkdir -p $COVERAGE_DIR
+
 echo "Run tests"
-go test ./... -coverprofile=coverage.out
+go test ./... -coverprofile $COVERAGE_DIR/coverage.out -covermode count
 
 echo "Create Coverage HTML"
-go tool cover -html=coverage.out
+go tool cover -html=$COVERAGE_DIR/coverage.out -o $COVERAGE_DIR/test-coverage.html
