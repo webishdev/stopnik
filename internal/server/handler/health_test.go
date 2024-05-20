f876c7613c6c8dfaecf8deacd1730e6c8c44e73a
Simon Skoczylas <github@giftkugel.de>
2024-05-20 13:35:56 +0200

    Add tests

--- internal/server/handler/health_test.go
+++ internal/server/handler/health_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"stopnik/internal/config"
@@ -8,6 +9,13 @@ import (
 	"stopnik/internal/store"
 	"testing"
 )
+
+var testHttpMethods = []string{
+	http.MethodPost,
+	http.MethodPut,
+	http.MethodPatch,
+	http.MethodDelete,
+}
 
 func Test_Health(t *testing.T) {
 	t.Run("Health without token", func(t *testing.T) {
@@ -36,6 +44,58 @@ func Test_Health(t *testing.T) {
 		}
 
 	})
+
+	t.Run("Health with token", func(t *testing.T) {
+		testConfig := createTestConfig(t)
+		tokenManager := store.NewTokenManager(testConfig, store.NewDefaultKeyLoader(testConfig))
+
+		client, clientExists := testConfig.GetClient("foo")
+		if !clientExists {
+			t.Error("client should exist")
+		}
+
+		tokenResponse := tokenManager.CreateAccessTokenResponse("foo", client, []string{"a:foo", "b:bar"})
+
+		healthHandler := NewHealthHandler(tokenManager)
+
+		httpRequest := &http.Request{
+			Method: http.MethodGet,
+			Header: http.Header{
+				internalHttp.Authorization: []string{"Bearer " + tokenResponse.AccessTokenKey},
+			},
+		}
+		rr := httptest.NewRecorder()
+
+		healthHandler.ServeHTTP(rr, httpRequest)
+
+		contentType := rr.Header().Get(internalHttp.ContentType)
+
+		if contentType != internalHttp.ContentTypeJSON {
+			t.Errorf("content type should be %s", internalHttp.ContentTypeJSON)
+		}
+
+		jsonString := rr.Body.String()
+
+		if jsonString != `{"ping":"pong","username":"foo","scopes":["a:foo","b:bar"]}` {
+			t.Errorf("json string should be %s, but was %s", `{"ping":"pong","username":"foo","scopes":["a:foo","b:bar"]}`, jsonString)
+		}
+
+	})
+
+	for _, method := range testHttpMethods {
+		testMessage := fmt.Sprintf("Health with token unsupported method %s", method)
+		t.Run(testMessage, func(t *testing.T) {
+			healthHandler := NewHealthHandler(&store.TokenManager{})
+
+			rr := httptest.NewRecorder()
+
+			healthHandler.ServeHTTP(rr, httptest.NewRequest(method, "/health", nil))
+
+			if rr.Code != http.StatusMethodNotAllowed {
+				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusMethodNotAllowed)
+			}
+		})
+	}
 }
 
 func createTestConfig(t *testing.T) *config.Config {
