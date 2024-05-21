7ee857a767148a47e5cdbf21068d302031fc5214
Simon Skoczylas <github@giftkugel.de>
2024-05-21 11:33:54 +0200

    Improve build

--- build.sh
+++ build.sh
@@ -48,12 +48,15 @@ function build() {
   DIR="$OS_NAME-$GO_ARCH"
   echo "Build $NICE_NAME version $VERSION ($GIT_HASH) for $GO_OS $GO_ARCH"
   GOOS=$GO_OS GOARCH=$GO_ARCH go build -ldflags="-s -w -X 'main.Version=$VERSION' -X 'main.GitHash=$GIT_HASH'" -o bin/$DIR/$FILE_NAME$FILE_EXTENSION main.go
-  echo "Create SHA256 sum for $GO_OS $GO_ARCH"
-  shasum -a 256  bin/$DIR/$FILE_NAME$FILE_EXTENSION >> bin/$DIR/sha256sum.txt
+
   CURRENT_DIR=$(pwd)
-  echo "Package into Zip"
   cd bin/$DIR
+
+  echo "Create SHA256 sum for $GO_OS $GO_ARCH"
+  shasum -a 256  $FILE_NAME$FILE_EXTENSION >> bin/$DIR/sha256sum.txt
+  echo "Package into Zip"
   zip -q -r ../${NAME}${VERSION}-${OS_NAME}-${GO_ARCH}.zip ./
+
   cd $CURRENT_DIR
   echo
 }
