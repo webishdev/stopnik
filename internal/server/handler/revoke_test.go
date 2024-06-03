dfa8d80ed2ff3d92d6d2a55b20417a2a512c1f0d
Simon Skoczylas <github@giftkugel.de>
2024-06-03 18:59:40 +0200

    Add tests

--- internal/server/handler/revoke_test.go
+++ internal/server/handler/revoke_test.go
@@ -10,22 +10,23 @@ import (
 	"testing"
 )
 
-func Test_Introspect(t *testing.T) {
-	var testInvalidIntrospectHttpMethods = []string{
+func Test_Revoke(t *testing.T) {
+
+	var testInvalidRevokeHttpMethods = []string{
 		http.MethodGet,
 		http.MethodPut,
 		http.MethodPatch,
 		http.MethodDelete,
 	}
 
-	for _, method := range testInvalidIntrospectHttpMethods {
-		testMessage := fmt.Sprintf("Introspect with unsupported method %s", method)
+	for _, method := range testInvalidRevokeHttpMethods {
+		testMessage := fmt.Sprintf("Revoke with unsupported method %s", method)
 		t.Run(testMessage, func(t *testing.T) {
-			introspectHandler := CreateIntrospectHandler(&config.Config{}, &validation.RequestValidator{}, &store.TokenManager{})
+			revokeHandler := CreateRevokeHandler(&config.Config{}, &validation.RequestValidator{}, &store.TokenManager{})
 
 			rr := httptest.NewRecorder()
 
-			introspectHandler.ServeHTTP(rr, httptest.NewRequest(method, "/introspect", nil))
+			revokeHandler.ServeHTTP(rr, httptest.NewRequest(method, "/introspect", nil))
 
 			if rr.Code != http.StatusMethodNotAllowed {
 				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusMethodNotAllowed)
