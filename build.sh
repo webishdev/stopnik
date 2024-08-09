1d25a663ffe6a73c06da578fb783af008a838219
Simon Skoczylas <github@giftkugel.de>
2024-08-09 17:28:38 +0200

    Update build

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ function build() {
   if [[ "$GO_OS" == "darwin" ]]; then
       OS_NAME="macos"
   fi
-  FILE_NAME="${CI_OS:-}$NAME$VERSION-$GO_ARCH"
+  FILE_NAME="$NAME$VERSION-$GO_ARCH"
   DIR="$OS_NAME-$GO_ARCH"
   echo "Build $NICE_NAME version $VERSION ($GIT_HASH) for $GO_OS $GO_ARCH into $DIR"
   GOOS=$GO_OS GOARCH=$GO_ARCH go build -ldflags="-s -w -X 'main.Version=$VERSION' -X 'main.GitHash=$GIT_HASH'" -o bin/$DIR/$FILE_NAME$FILE_EXTENSION main.go
@@ -59,7 +59,7 @@ function build() {
   echo "Create SHA256 sum for $GO_OS $GO_ARCH"
   shasum -a 256  $FILE_NAME$FILE_EXTENSION >> sha256sum.txt
   echo "Package into ZIP: ${NAME}${VERSION}-${OS_NAME}-${GO_ARCH}.zip"
-  zip -q -r ../${NAME}${VERSION}-${OS_NAME}-${GO_ARCH}.zip ./
+  zip -q -r ../"${CI_OS:-}${NAME}${VERSION}-${OS_NAME}-${GO_ARCH}.zip"" ./
 
   cd $CURRENT_DIR
 
