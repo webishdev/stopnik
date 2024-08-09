8c9860ac2596a81daed48ce90596da5d04a6887b
Simon Skoczylas <github@giftkugel.de>
2024-08-09 14:06:50 +0200

    Refactor

--- internal/server/handler/authorize.go
+++ internal/server/handler/authorize.go
@@ -36,173 +36,114 @@ func CreateAuthorizeHandler(validator *validation.RequestValidator, cookieManage
 func (handler *AuthorizeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.AccessLogRequest(r)
 	if r.Method == http.MethodGet {
-		clientId := r.URL.Query().Get(oauth2.ParameterClientId)
-		client, exists := handler.validator.ValidateClientId(clientId)
-		if !exists {
-			log.Error("Invalid client id %s", clientId)
-			BadRequestHandler(w, r)
+		if handler.handleGetRequest(w, r) {
 			return
-		}
-
-		redirect := r.URL.Query().Get(oauth2.ParameterRedirectUri)
-
-		redirectURL, urlParseError := url.Parse(redirect)
-		if urlParseError != nil {
-			log.Error("Could not parse redirect URI %s for client %s", redirect, client.Id)
-			BadRequestHandler(w, r)
-			return
-		}
-
-		invalidRedirectErrorHandler := validateRedirect(client, redirect)
-		if invalidRedirectErrorHandler != nil {
-			invalidRedirectErrorHandler(w, r)
-			return
-		}
-
-		state := r.URL.Query().Get(oauth2.ParameterState)
-
-		responseTypeQueryParameter := r.URL.Query().Get(oauth2.ParameterResponseType)
-		responseType, valid := oauth2.ResponseTypeFromString(responseTypeQueryParameter)
-		if !valid {
-			log.Error("Invalid %s parameter with value %s for client %s", oauth2.ParameterResponseType, responseTypeQueryParameter, client.Id)
-
-			errorMessage := fmt.Sprintf("Invalid %s parameter value", oauth2.ParameterResponseType)
-			authorizeError := &oauth2.ErrorResponseParameter{Error: oauth2.EtInvalidRequest, Description: errorMessage}
-			oauth2.ErrorResponseHandler(w, redirectURL, state, authorizeError)
-			return
-		}
-
-		scope := r.URL.Query().Get(oauth2.ParameterScope)
-
-		codeChallenge := ""
-		codeChallengeMethod := ""
-		if responseType == oauth2.RtCode {
-			codeChallenge = r.URL.Query().Get(pkce.ParameterCodeChallenge)
-			codeChallengeMethod = r.URL.Query().Get(pkce.ParameterCodeChallengeMethod)
-		}
-
-		if log.IsDebug() {
-			log.Debug("Response type: %s", responseType)
-			log.Debug("Redirect URI: %s", redirect)
-			log.Debug("State: %s", state)
-			log.Debug("Scope: %s", scope)
-		}
-
-		scopes := strings.Split(scope, " ")
-
-		id := uuid.New()
-		authSession := &store.AuthSession{
-			Id:                  id.String(),
-			Redirect:            redirect,
-			AuthURI:             r.URL.RequestURI(),
-			CodeChallenge:       codeChallenge,
-			CodeChallengeMethod: codeChallengeMethod,
-			ClientId:            clientId,
-			ResponseType:        string(responseType),
-			Scopes:              scopes,
-			State:               state,
-		}
-
-		handler.sessionManager.StartSession(authSession)
-
-		user, validCookie := handler.cookieManager.ValidateCookie(r)
-
-		if validCookie {
-			authSession.Username = user.Username
-
-			query := redirectURL.Query()
-
-			if responseType == oauth2.RtToken {
-				accessTokenResponse := handler.tokenManager.CreateAccessTokenResponse(user.Username, client, scopes)
-				setImplicitGrantParameter(query, accessTokenResponse)
-			} else if responseType == oauth2.RtCode {
-				setAuthorizationGrantParameter(query, id.String())
-			} else {
-				log.Error("Invalid response type %s", responseType)
-				oauth2.ErrorResponseHandler(w, redirectURL, state, &oauth2.ErrorResponseParameter{Error: oauth2.EtUnsupportedResponseType})
-				return
-			}
-
-			if state != "" {
-				query.Set(oauth2.ParameterState, state)
-			}
-
-			redirectURL.RawQuery = query.Encode()
-
-			w.Header().Set(internalHttp.Location, redirectURL.String())
-			w.WriteHeader(http.StatusFound)
-		} else {
-			// http.ServeFile(w, r, "foo.html")
-			// bytes := []byte(loginHtml)
-
-			// Show login page
-			query := r.URL.Query()
-			encodedQuery := query.Encode()
-			formAction := fmt.Sprintf("authorize?%s", encodedQuery)
-			loginTemplate := template.LoginTemplate(authSession.Id, formAction)
-
-			_, err := w.Write(loginTemplate.Bytes())
-			if err != nil {
-				InternalServerErrorHandler(w, r)
-				return
-			}
 		}
 	} else if r.Method == http.MethodPost {
-		// Handle Post from Login
-		user, userExists := handler.validator.ValidateFormLogin(r)
-		if !userExists {
-			w.Header().Set(internalHttp.Location, r.RequestURI)
-			w.WriteHeader(http.StatusSeeOther)
+		user, failed := handler.validateLogin(w, r)
+		if failed {
 			return
 		}
 
-		cookie, err := handler.cookieManager.CreateCookie(user.Username)
-		if err != nil {
-			InternalServerErrorHandler(w, r)
+		if handler.handlePostRequest(w, r, user) {
 			return
 		}
+	} else {
+		MethodNotAllowedHandler(w, r)
+		return
+	}
+}
 
-		http.SetCookie(w, &cookie)
+func (handler *AuthorizeHandler) handleGetRequest(w http.ResponseWriter, r *http.Request) bool {
+	clientId := r.URL.Query().Get(oauth2.ParameterClientId)
+	client, exists := handler.validator.ValidateClientId(clientId)
+	if !exists {
+		log.Error("Invalid client id %s", clientId)
+		BadRequestHandler(w, r)
+		return true
+	}
 
-		authSessionForm := r.PostFormValue("stopnik_auth_session")
-		authSession, exists := handler.sessionManager.GetSession(authSessionForm)
-		if !exists {
-			w.Header().Set(internalHttp.Location, r.RequestURI)
-			w.WriteHeader(http.StatusSeeOther)
-			return
-		}
+	redirect := r.URL.Query().Get(oauth2.ParameterRedirectUri)
 
+	redirectURL, urlParseError := url.Parse(redirect)
+	if urlParseError != nil {
+		log.Error("Could not parse redirect URI %s for client %s", redirect, client.Id)
+		BadRequestHandler(w, r)
+		return true
+	}
+
+	invalidRedirectErrorHandler := validateRedirect(client, redirect)
+	if invalidRedirectErrorHandler != nil {
+		invalidRedirectErrorHandler(w, r)
+		return true
+	}
+
+	state := r.URL.Query().Get(oauth2.ParameterState)
+
+	responseTypeQueryParameter := r.URL.Query().Get(oauth2.ParameterResponseType)
+	responseType, valid := oauth2.ResponseTypeFromString(responseTypeQueryParameter)
+	if !valid {
+		log.Error("Invalid %s parameter with value %s for client %s", oauth2.ParameterResponseType, responseTypeQueryParameter, client.Id)
+
+		errorMessage := fmt.Sprintf("Invalid %s parameter value", oauth2.ParameterResponseType)
+		authorizeError := &oauth2.ErrorResponseParameter{Error: oauth2.EtInvalidRequest, Description: errorMessage}
+		oauth2.ErrorResponseHandler(w, redirectURL, state, authorizeError)
+		return true
+	}
+
+	scope := r.URL.Query().Get(oauth2.ParameterScope)
+
+	codeChallenge := ""
+	codeChallengeMethod := ""
+	if responseType == oauth2.RtCode {
+		codeChallenge = r.URL.Query().Get(pkce.ParameterCodeChallenge)
+		codeChallengeMethod = r.URL.Query().Get(pkce.ParameterCodeChallengeMethod)
+	}
+
+	if log.IsDebug() {
+		log.Debug("Response type: %s", responseType)
+		log.Debug("Redirect URI: %s", redirect)
+		log.Debug("State: %s", state)
+		log.Debug("Scope: %s", scope)
+	}
+
+	scopes := strings.Split(scope, " ")
+
+	id := uuid.New()
+	authSession := &store.AuthSession{
+		Id:                  id.String(),
+		Redirect:            redirect,
+		AuthURI:             r.URL.RequestURI(),
+		CodeChallenge:       codeChallenge,
+		CodeChallengeMethod: codeChallengeMethod,
+		ClientId:            clientId,
+		ResponseType:        string(responseType),
+		Scopes:              scopes,
+		State:               state,
+	}
+
+	handler.sessionManager.StartSession(authSession)
+
+	user, validCookie := handler.cookieManager.ValidateCookie(r)
+
+	if validCookie {
 		authSession.Username = user.Username
-		redirectURL, urlParseError := url.Parse(authSession.Redirect)
-		if urlParseError != nil {
-			InternalServerErrorHandler(w, r)
-			return
-		}
-
-		responseType, valid := oauth2.ResponseTypeFromString(authSession.ResponseType)
-		if !valid {
-			InternalServerErrorHandler(w, r)
-			return
-		}
 
 		query := redirectURL.Query()
+
 		if responseType == oauth2.RtToken {
-			client, exists := handler.validator.ValidateClientId(authSession.ClientId)
-			if !exists {
-				InternalServerErrorHandler(w, r)
-				return
-			}
-			accessTokenResponse := handler.tokenManager.CreateAccessTokenResponse(user.Username, client, authSession.Scopes)
+			accessTokenResponse := handler.tokenManager.CreateAccessTokenResponse(user.Username, client, scopes)
 			setImplicitGrantParameter(query, accessTokenResponse)
 		} else if responseType == oauth2.RtCode {
-			setAuthorizationGrantParameter(query, authSessionForm)
+			setAuthorizationGrantParameter(query, id.String())
 		} else {
-			BadRequestHandler(w, r)
-			return
+			log.Error("Invalid response type %s", responseType)
+			oauth2.ErrorResponseHandler(w, redirectURL, state, &oauth2.ErrorResponseParameter{Error: oauth2.EtUnsupportedResponseType})
+			return true
 		}
 
-		if authSession.State != "" {
-			query.Set(oauth2.ParameterState, authSession.State)
+		if state != "" {
+			query.Set(oauth2.ParameterState, state)
 		}
 
 		redirectURL.RawQuery = query.Encode()
@@ -210,9 +151,90 @@ func (handler *AuthorizeHandler) ServeHTTP(w http.ResponseWriter, r *http.Reques
 		w.Header().Set(internalHttp.Location, redirectURL.String())
 		w.WriteHeader(http.StatusFound)
 	} else {
-		MethodNotAllowedHandler(w, r)
-		return
+		// http.ServeFile(w, r, "foo.html")
+		// bytes := []byte(loginHtml)
+
+		// Show login page
+		query := r.URL.Query()
+		encodedQuery := query.Encode()
+		formAction := fmt.Sprintf("authorize?%s", encodedQuery)
+		loginTemplate := template.LoginTemplate(authSession.Id, formAction)
+
+		_, err := w.Write(loginTemplate.Bytes())
+		if err != nil {
+			InternalServerErrorHandler(w, r)
+			return true
+		}
 	}
+	return false
+}
+
+func (handler *AuthorizeHandler) handlePostRequest(w http.ResponseWriter, r *http.Request, user *config.User) bool {
+	cookie, err := handler.cookieManager.CreateCookie(user.Username)
+	if err != nil {
+		InternalServerErrorHandler(w, r)
+		return true
+	}
+
+	http.SetCookie(w, &cookie)
+
+	authSessionForm := r.PostFormValue("stopnik_auth_session")
+	authSession, exists := handler.sessionManager.GetSession(authSessionForm)
+	if !exists {
+		w.Header().Set(internalHttp.Location, r.RequestURI)
+		w.WriteHeader(http.StatusSeeOther)
+		return true
+	}
+
+	authSession.Username = user.Username
+	redirectURL, urlParseError := url.Parse(authSession.Redirect)
+	if urlParseError != nil {
+		InternalServerErrorHandler(w, r)
+		return true
+	}
+
+	responseType, valid := oauth2.ResponseTypeFromString(authSession.ResponseType)
+	if !valid {
+		InternalServerErrorHandler(w, r)
+		return true
+	}
+
+	query := redirectURL.Query()
+	if responseType == oauth2.RtToken {
+		client, exists := handler.validator.ValidateClientId(authSession.ClientId)
+		if !exists {
+			InternalServerErrorHandler(w, r)
+			return true
+		}
+		accessTokenResponse := handler.tokenManager.CreateAccessTokenResponse(user.Username, client, authSession.Scopes)
+		setImplicitGrantParameter(query, accessTokenResponse)
+	} else if responseType == oauth2.RtCode {
+		setAuthorizationGrantParameter(query, authSessionForm)
+	} else {
+		BadRequestHandler(w, r)
+		return true
+	}
+
+	if authSession.State != "" {
+		query.Set(oauth2.ParameterState, authSession.State)
+	}
+
+	redirectURL.RawQuery = query.Encode()
+
+	w.Header().Set(internalHttp.Location, redirectURL.String())
+	w.WriteHeader(http.StatusFound)
+	return false
+}
+
+func (handler *AuthorizeHandler) validateLogin(w http.ResponseWriter, r *http.Request) (*config.User, bool) {
+	// Handle Post from Login
+	user, userExists := handler.validator.ValidateFormLogin(r)
+	if !userExists {
+		w.Header().Set(internalHttp.Location, r.RequestURI)
+		w.WriteHeader(http.StatusSeeOther)
+		return nil, true
+	}
+	return user, false
 }
 
 func validateRedirect(client *config.Client, redirect string) func(w http.ResponseWriter, r *http.Request) {
