3682702391a37844ab6b762352f4eec9c83cffe0
Simon Skoczylas <github@giftkugel.de>
2024-08-13 12:38:15 +0200

    Add tests

--- internal/server/handler/account_test.go
+++ internal/server/handler/account_test.go
@@ -2,16 +2,170 @@ package handler
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"stopnik/internal/config"
 	internalHttp "stopnik/internal/http"
 	"stopnik/internal/server/validation"
+	"strings"
 	"testing"
 )
 
 func Test_Account(t *testing.T) {
 
+	testConfig := &config.Config{
+		Clients: []config.Client{
+			{
+				Id:        "foo",
+				Secret:    "d82c4eb5261cb9c8aa9855edd67d1bd10482f41529858d925094d173fa662aa91ff39bc5b188615273484021dfb16fd8284cf684ccf0fc795be3aa2fc1e6c181",
+				Redirects: []string{"https://example.com/callback"},
+			},
+		},
+		Users: []config.User{
+			{
+				Username: "foo",
+				Password: "d82c4eb5261cb9c8aa9855edd67d1bd10482f41529858d925094d173fa662aa91ff39bc5b188615273484021dfb16fd8284cf684ccf0fc795be3aa2fc1e6c181",
+			},
+		},
+	}
+
+	err := testConfig.Setup()
+	if err != nil {
+		t.Error(err)
+	}
+
+	testAccountWithoutCookie(t, testConfig)
+
+	testAccountWithCookie(t, testConfig)
+
+	testAccountLogin(t, testConfig)
+
 	testAccountNotAllowedHttpMethods(t)
+}
+
+func testAccountWithoutCookie(t *testing.T, testConfig *config.Config) {
+	t.Run("Account without cookie", func(t *testing.T) {
+		requestValidator := validation.NewRequestValidator(testConfig)
+		cookieManager := internalHttp.NewCookieManager(testConfig)
+
+		accountHandler := CreateAccountHandler(requestValidator, cookieManager)
+
+		rr := httptest.NewRecorder()
+
+		accountHandler.ServeHTTP(rr, httptest.NewRequest(http.MethodGet, "/account", nil))
+
+		if rr.Code != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
+		}
+
+		contentType := rr.Header().Get(internalHttp.ContentType)
+
+		if contentType != "text/html; charset=utf-8" {
+			t.Errorf("content type was not text/html: %v", contentType)
+		}
+
+		response := rr.Result()
+		body, bodyReadErr := io.ReadAll(response.Body)
+
+		if bodyReadErr != nil {
+			t.Errorf("could not read response body: %v", bodyReadErr)
+		}
+
+		if body == nil {
+			t.Errorf("response body was nil")
+		}
+	})
+}
+
+func testAccountWithCookie(t *testing.T, testConfig *config.Config) {
+	t.Run("Account cookie", func(t *testing.T) {
+		requestValidator := validation.NewRequestValidator(testConfig)
+		cookieManager := internalHttp.NewCookieManager(testConfig)
+
+		user, _ := testConfig.GetUser("foo")
+		cookie, _ := cookieManager.CreateCookie(user.Username)
+
+		accountHandler := CreateAccountHandler(requestValidator, cookieManager)
+
+		rr := httptest.NewRecorder()
+
+		request := httptest.NewRequest(http.MethodGet, "/account", nil)
+		request.AddCookie(&cookie)
+
+		accountHandler.ServeHTTP(rr, request)
+
+		if rr.Code != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
+		}
+
+		contentType := rr.Header().Get(internalHttp.ContentType)
+
+		if contentType != "text/html; charset=utf-8" {
+			t.Errorf("content type was not text/html: %v", contentType)
+		}
+
+		response := rr.Result()
+		body, bodyReadErr := io.ReadAll(response.Body)
+
+		if bodyReadErr != nil {
+			t.Errorf("could not read response body: %v", bodyReadErr)
+		}
+
+		if body == nil {
+			t.Errorf("response body was nil")
+		}
+	})
+}
+
+func testAccountLogin(t *testing.T, testConfig *config.Config) {
+	type loginParameter struct {
+		username string
+		password string
+	}
+	var loginParameters = []loginParameter{
+		{"foo", "bar"},
+		{"xxx", "zzz"},
+	}
+	for _, test := range loginParameters {
+		testMessage := fmt.Sprintf("Account login %v", test.username)
+
+		t.Run(testMessage, func(t *testing.T) {
+			requestValidator := validation.NewRequestValidator(testConfig)
+			cookieManager := internalHttp.NewCookieManager(testConfig)
+
+			cookie, _ := cookieManager.CreateCookie(test.username)
+
+			accountHandler := CreateAccountHandler(requestValidator, cookieManager)
+
+			rr := httptest.NewRecorder()
+
+			bodyString := testCreateBody(
+				"stopnik_username", test.username,
+				"stopnik_password", test.password,
+			)
+			body := strings.NewReader(bodyString)
+
+			request := httptest.NewRequest(http.MethodPost, "/account", body)
+			request.Header.Add(internalHttp.ContentType, "application/x-www-form-urlencoded")
+			request.AddCookie(&cookie)
+
+			accountHandler.ServeHTTP(rr, request)
+
+			if rr.Code != http.StatusSeeOther {
+				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusSeeOther)
+			}
+
+			location, locationError := rr.Result().Location()
+			if locationError != nil {
+				t.Errorf("location was not provied: %v", locationError)
+			}
+
+			if location.String() != "/account" {
+				t.Errorf("handler returned wrong location: got %v want %v", location.String(), "/account")
+			}
+		})
+	}
 }
 
 func testAccountNotAllowedHttpMethods(t *testing.T) {
