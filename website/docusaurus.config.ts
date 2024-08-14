1cf025f2a823b60b8933862c93b4998802fdefea
Simon Skoczylas <github@giftkugel.de>
2024-08-14 19:05:56 +0200

    Update documentation

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/webishdev/stopnik/tree/main/website',
         },
         blog: {
           showReadingTime: true,
@@ -64,7 +64,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/stopnik-social-card.jpg',
     navbar: {
       title: 'STOPnik',
       logo: {
