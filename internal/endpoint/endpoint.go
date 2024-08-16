ede5984147a22514391018c463b656122540535b
Simon Skoczylas <github@giftkugel.de>
2024-08-16 22:29:41 +0200

    Fix lint error

--- internal/endpoint/endpoint.go
+++ internal/endpoint/endpoint.go
@@ -2,12 +2,12 @@ package endpoint
 
 const (
 	Authorization string = "/authorize"
-	Token                = "/token"
-	Health               = "/health"
-	Account              = "/account"
-	Logout               = "/logout"
-	Introspect           = "/introspect"
-	Revoke               = "/revoke"
-	Metadata             = "/.well-known/oauth-authorization-server"
-	Keys                 = "/keys"
+	Token         string = "/token"
+	Health        string = "/health"
+	Account       string = "/account"
+	Logout        string = "/logout"
+	Introspect    string = "/introspect"
+	Revoke        string = "/revoke"
+	Metadata      string = "/.well-known/oauth-authorization-server"
+	Keys          string = "/keys"
 )
