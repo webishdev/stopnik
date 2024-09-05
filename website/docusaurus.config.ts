47df05a9167a312dea54c8c64e397b868e80f0a5
Simon Skoczylas <github@giftkugel.de>
2024-09-05 18:45:30 +0200

    docusaurus.config.ts aktualisieren

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://stopnik.webis.dev',
+  url: 'https://stopnik.webish.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
