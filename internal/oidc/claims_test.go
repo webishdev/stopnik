ee1c76ab47cc49e978735fe7de2888ccc5872859
Simon Skoczylas <github@giftkugel.de>
2024-10-06 17:42:25 +0200

    Add test

--- internal/oidc/claims_test.go
+++ internal/oidc/claims_test.go
@@ -26,3 +26,36 @@ func Test_Claims(t *testing.T) {
 		})
 	}
 }
+
+func Test_HasUserInfoClaim(t *testing.T) {
+	claimsParameter := &ClaimsParameter{}
+	claimsParameter.UserInfo = make(map[string]*ClaimsParameterMember)
+	claimsParameter.UserInfo["foo"] = &ClaimsParameterMember{}
+
+	type claimParameter struct {
+		name     string
+		expected bool
+	}
+
+	var claimParameters = []claimParameter{
+		{"foo", true},
+		{"bar", false},
+	}
+
+	for _, test := range claimParameters {
+		testMessage := fmt.Sprintf("OIDC has claim %s", test.name)
+		t.Run(testMessage, func(t *testing.T) {
+			exists := HasUserInfoClaim(claimsParameter, test.name)
+			if exists != test.expected {
+				t.Errorf("assertion error, %v != %v", exists, test.expected)
+			}
+		})
+	}
+}
+
+func Test_HasUserInfoClaimNoValue(t *testing.T) {
+	exists := HasUserInfoClaim(nil, "foo")
+	if exists {
+		t.Errorf("claim should not exist")
+	}
+}
